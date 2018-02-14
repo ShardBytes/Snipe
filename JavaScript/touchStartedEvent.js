2c3de36b0a723dca468a8a0971d58f5cdd8c7982
Plasmoxy <sebohacker@gmail.com>
2018-02-14 23:34:32 +0100

    useful things

--- JavaScript/touchStartedEvent.js
+++ JavaScript/touchStartedEvent.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){ // on page load
+window.addEventListener('load', function(){
 
     document.body.addEventListener('touchstart', function(e){
         // example : alert(e.changedTouches[0].pageX) // alert pageX coordinate of touch point
