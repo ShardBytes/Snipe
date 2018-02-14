649f4e6c84aaa26f21180e88351b7c0954e62808
Plasmoxy <sebohacker@gmail.com>
2018-02-14 23:07:15 +0100

    added some useful js stuff

---
+++ JavaScript/detectAndroid.js
@@ -0,0 +1,2 @@
+var ua = navigator.userAgent.toLowerCase();
+var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
---
+++ JavaScript/touchStartedEvent.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){ // on page load
+
+    document.body.addEventListener('touchstart', function(e){
+        // example : alert(e.changedTouches[0].pageX) // alert pageX coordinate of touch point
+    }, false)
+
+}, false)
